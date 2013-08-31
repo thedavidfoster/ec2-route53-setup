8902fc2af3fabf0a31f13bbd07d2863a7299e8b4
David Foster <david.foster@pearson.com>
2013-08-30 22:26:16 -0700

    fixed link creation

--- setup-cli53-ec2.sh
+++ setup-cli53-ec2.sh
@@ -10,4 +10,5 @@ chmod 600 /etc/route53/config
 
 wget https://raw.github.com/thedavidfoster/ec2-route53-setup/master/update-route53-dns.sh
 mv update-route53-dns.sh /usr/sbin/update-route53-dns
-ln -s /etc/dhcp3/dhclient-exit-hooks.d/update-route53-dns /usr/sbin/update-route53-dns
+
+ln -s /usr/sbin/update-route53-dns /etc/dhcp/dhclient-exit-hooks.d/update-route53-dns 
